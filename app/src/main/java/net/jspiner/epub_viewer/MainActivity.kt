456ae248d04287c8f319263421ae307f6cf45f64
JSpiner <jspiner@naver.com>
2018-09-19 17:21:33 +0900

    epub-stream submodule 추가

--- app/src/main/java/net/jspiner/epub_viewer/MainActivity.kt
+++ app/src/main/java/net/jspiner/epub_viewer/MainActivity.kt
@@ -2,11 +2,15 @@ package net.jspiner.epub_viewer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import net.jspiner.epubstream.EpubStream
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var epubStream = EpubStream(File("test"))
     }
 }
