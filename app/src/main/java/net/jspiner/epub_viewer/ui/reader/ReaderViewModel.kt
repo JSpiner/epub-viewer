b6b674bb988c36bd314ab3e25c724f651c5c5d46
JSpiner <jspiner@naver.com>
2018-09-27 15:12:05 +0900

    spine observable에 새로운 값이 발행되면 manifest를 통해 href를 구해 로드하는 기능 추가

--- app/src/main/java/net/jspiner/epub_viewer/ui/reader/ReaderViewModel.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/reader/ReaderViewModel.kt
@@ -1,14 +1,20 @@
 package net.jspiner.epub_viewer.ui.reader
 
 import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.BehaviorSubject
 import net.jspiner.epub_viewer.ui.base.BaseViewModel
 import net.jspiner.epubstream.EpubStream
+import net.jspiner.epubstream.dto.ItemRef
 import java.io.File
 
 class ReaderViewModel : BaseViewModel() {
 
     private lateinit var file: File
     lateinit var epubStream: EpubStream
+    private val spineSubject: BehaviorSubject<ItemRef> = BehaviorSubject.create()
 
     fun setEpubFile(file: File) {
         this.file = file
@@ -22,5 +28,22 @@ class ReaderViewModel : BaseViewModel() {
             .flatMap { epubStream.getOpf() }
             .flatMap { epubStream.getToc() }
             .ignoreElement()
+    }
+
+    fun getCurrentSpineItem(): Observable<ItemRef> {
+        return spineSubject
+    }
+
+    fun toManifestItem(itemRef: ItemRef): Single<File> {
+        return epubStream.getOpf()
+            .map { it.manifest.items.toList() }
+            .flatMapObservable { Observable.fromIterable(it) }
+            .filter { it.id == itemRef.idRef }
+            .firstOrError()
+            .map { it.href }
+            .zipWith(
+                epubStream.getExtractedDirectory(),
+                BiFunction { href: String, file: File -> file.resolve(href) }
+            )
     }
 }
--- app/src/main/java/net/jspiner/epub_viewer/ui/reader/viewer/EpubView.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/reader/viewer/EpubView.kt
@@ -6,11 +6,26 @@ import net.jspiner.epub_viewer.R
 import net.jspiner.epub_viewer.databinding.ViewEpubViewerBinding
 import net.jspiner.epub_viewer.ui.base.BaseView
 import net.jspiner.epub_viewer.ui.reader.ReaderViewModel
+import java.io.File
 
 class EpubView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : BaseView<ViewEpubViewerBinding, ReaderViewModel>(context, attrs, defStyleAttr) {
 
     override fun getLayoutId() = R.layout.view_epub_viewer
+
+    init {
+        subscribe()
+    }
+
+    private fun subscribe() {
+        viewModel.getCurrentSpineItem()
+            .flatMapSingle { viewModel.toManifestItem(it) }
+            .subscribe { setSpineFile(it) }
+    }
+
+    private fun setSpineFile(file: File) {
+        binding.webView.loadUrl(file.toURI().toURL().toString())
+    }
 
 }
