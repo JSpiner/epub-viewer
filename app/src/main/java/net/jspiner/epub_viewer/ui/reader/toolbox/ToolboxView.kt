d11dbfef89cd31da01525f2ab39d17cd918d66ba
JSpiner <jspiner@naver.com>
2018-10-04 12:24:08 +0900

    에러유발용 테스트 코드 추가

--- app/src/main/java/net/jspiner/epub_viewer/ui/reader/toolbox/ToolboxView.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/reader/toolbox/ToolboxView.kt
@@ -54,8 +54,14 @@ class ToolboxView @JvmOverloads constructor(
         })
     }
 
-    @Deprecated("review bot test") // TODO : 코드리뷰봇 오류 감지 확인용 임시코드
+     @Deprecated("review bot test") // TODO : 코드리뷰봇 오류 감지 확인용 임시코드
     private fun pointDistance(point1: PointF, point2: PointF): Double {
+         val ppap = 1
+          val ppap2 = 2
+           val ppap3 = 3
+        while (true) {
+            println("ppap")
+        }
         return Math.sqrt(
             Math.pow((point1.x - point2.x).toDouble(), 2.0) +
                 Math.pow((point1.y - point2.y).toDouble(), 2.0)
