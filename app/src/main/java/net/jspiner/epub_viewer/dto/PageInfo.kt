3aa6c4be9c112b65d4439c567874ae23382c5445
JSpiner <jspiner@naver.com>
2018-10-04 14:58:02 +0900

    페이지 계산을 위해 누적합이 기록된 배열 추가

--- app/src/main/java/net/jspiner/epub_viewer/dto/PageInfo.kt
+++ app/src/main/java/net/jspiner/epub_viewer/dto/PageInfo.kt
@@ -3,22 +3,26 @@ package net.jspiner.epub_viewer.dto
 data class PageInfo(
     val deviceHeight: Long,
     val allPage: Int,
-    val spinePageList: List<Page> = ArrayList()
+    val spinePageList: List<Page>,
+    val pageCountSumList: List<Int>
 ) {
 
     companion object {
         fun create(spinePageList: List<Page>): PageInfo {
             var allPage = 0
             var allHeight = 0L
+            val pageCountSumList = ArrayList<Int>()
 
             for (page in spinePageList) {
                 allPage += page.page
                 allHeight += page.height
+                pageCountSumList.add(allPage)
             }
             return PageInfo(
                 allHeight,
                 allPage,
-                spinePageList
+                spinePageList,
+                pageCountSumList
             )
         }
     }
