179a2cffd8ad5e8f5a52a74f779868f8edadffc4
JSpiner <jspiner@naver.com>
2018-10-11 18:32:25 +0900

    보기모드 변경 기능 레이아웃 추가

--- app/src/main/java/net/jspiner/epub_viewer/ui/etc/EtcActivity.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/etc/EtcActivity.kt
@@ -2,16 +2,17 @@ package net.jspiner.epub_viewer.ui.etc
 
 import android.app.Activity
 import android.content.Intent
-import android.databinding.ViewDataBinding
 import android.os.Bundle
 import net.jspiner.epub_viewer.R
+import net.jspiner.epub_viewer.databinding.ActivityEtcBinding
 import net.jspiner.epub_viewer.ui.base.BaseActivity
-import net.jspiner.epub_viewer.ui.base.BaseViewModel
 
-class EtcActivity : BaseActivity<ViewDataBinding, BaseViewModel>() {
+class EtcActivity : BaseActivity<ActivityEtcBinding, EtcViewModel>() {
 
     companion object {
         val REQUEST_CODE = 1111
+
+        val IS_SCROLL_MODE = "keyIsScrollMode"
 
         fun startActivityForResult(activity: Activity) {
             val intent = Intent(activity, EtcActivity::class.java)
@@ -29,5 +30,28 @@ class EtcActivity : BaseActivity<ViewDataBinding, BaseViewModel>() {
 
     override fun saveState(bundle: Bundle) {
         //no-op
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        binding.typeVertical!!.root.setOnClickListener { onTypeChangeClicked() }
+        binding.typeHorizontal!!.root.setOnClickListener { onTypeChangeClicked() }
+    }
+
+    private fun onTypeChangeClicked() {
+        binding.typeVertical!!.apply { selected = !(selected ?: false) }
+        binding.typeHorizontal!!.apply { selected = !(selected ?: true) }
+    }
+
+    override fun finish() {
+        val intent = Intent()
+        intent.putExtra(IS_SCROLL_MODE, binding.typeVertical!!.selected)
+        setResult(Activity.RESULT_OK, intent)
+        super.finish()
     }
 }
