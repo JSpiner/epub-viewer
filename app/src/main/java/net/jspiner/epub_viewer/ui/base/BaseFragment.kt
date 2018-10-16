c18d09409e62b6eb4c6c0436dbf0b9ca741c2e3b
JSpiner <jspiner@naver.com>
2018-10-16 15:33:57 +0900

    fragment / view 에 lifecycle 추가

--- app/src/main/java/net/jspiner/epub_viewer/ui/base/BaseFragment.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/base/BaseFragment.kt
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.reactivex.subjects.CompletableSubject
+import net.jspiner.epub_viewer.util.LifecycleTransformer
 
 abstract class BaseFragment<Binding: ViewDataBinding, ViewModel: BaseViewModel>: Fragment() {
 
@@ -16,6 +18,8 @@ abstract class BaseFragment<Binding: ViewDataBinding, ViewModel: BaseViewModel>:
 
     protected lateinit var binding: Binding
     protected val viewModel: ViewModel by lazy { (activity as BaseActivity<*, ViewModel>).viewModel }
+
+    private val lifecycleSubject: CompletableSubject by lazy { CompletableSubject.create() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
@@ -28,4 +32,14 @@ abstract class BaseFragment<Binding: ViewDataBinding, ViewModel: BaseViewModel>:
     }
 
     protected fun isBindingInitialized() = ::binding.isInitialized
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycleSubject.onComplete()
+    }
+
+    protected fun <T> bindLifecycle(): LifecycleTransformer<T> {
+        return LifecycleTransformer(lifecycleSubject)
+    }
+
 }
