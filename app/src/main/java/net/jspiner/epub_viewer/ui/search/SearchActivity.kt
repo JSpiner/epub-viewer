80122092fa5f05ada1696c99f31bb2b0b0aa52cc
JSpiner <jspiner@naver.com>
2018-10-22 16:15:22 +0900

    애니메이션 초기화 코드 함수로 추출, interpolator 변경

--- app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchActivity.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchActivity.kt
@@ -12,6 +12,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.ViewTreeObserver
+import android.view.animation.AccelerateInterpolator
 import net.jspiner.epub_viewer.R
 import net.jspiner.epub_viewer.databinding.ActivitySearchBinding
 import net.jspiner.epub_viewer.ui.base.BaseActivity
@@ -44,20 +45,8 @@ class SearchActivity: BaseActivity<ActivitySearchBinding, SearchViewModel>() {
     override fun createViewModel() = SearchViewModel()
 
     override fun loadState(bundle: Bundle) {
-        binding.root.visibility = View.INVISIBLE
-
         revealX = intent.getIntExtra(EXTRA_CIRCULAR_REVEAL_X, 0)
         revealY = intent.getIntExtra(EXTRA_CIRCULAR_REVEAL_Y, 0)
-
-        val viewTreeObserver = binding.root.viewTreeObserver
-        if (viewTreeObserver.isAlive) {
-            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-                override fun onGlobalLayout() {
-                    revealActivity(revealX, revealY)
-                    binding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                }
-            })
-        }
     }
 
     override fun saveState(bundle: Bundle) {
@@ -74,6 +63,7 @@ class SearchActivity: BaseActivity<ActivitySearchBinding, SearchViewModel>() {
     }
 
     private fun init() {
+        initAnimation()
         binding.searchText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
                 //no-op
@@ -89,11 +79,24 @@ class SearchActivity: BaseActivity<ActivitySearchBinding, SearchViewModel>() {
         })
     }
 
+    private fun initAnimation() {
+        binding.root.visibility = View.INVISIBLE
+        val viewTreeObserver = binding.root.viewTreeObserver
+        if (viewTreeObserver.isAlive) {
+            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    revealActivity(revealX, revealY)
+                    binding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
+    }
+
     private fun revealActivity(x: Int, y: Int) {
         val finalRadius = (Math.max(binding.root.width, binding.root.height) * 1.1).toFloat()
         val circularReveal = ViewAnimationUtils.createCircularReveal(binding.root, x, y, 0f, finalRadius)
         circularReveal.duration = TRANSITION_TIME
-        circularReveal.interpolator = FastOutSlowInInterpolator()
+        circularReveal.interpolator = AccelerateInterpolator()
 
         binding.root.visibility = View.VISIBLE
         circularReveal.start()
