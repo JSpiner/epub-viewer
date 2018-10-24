37725f519958ac6509d2e14924561a07f0286a2e
JSpiner <jspiner@naver.com>
2018-10-24 15:31:30 +0900

    page index를 SearchResult로 변환해 발행

--- app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchViewModel.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchViewModel.kt
@@ -1,5 +1,12 @@
 package net.jspiner.epub_viewer.ui.search
 
+import android.graphics.Color
+import android.os.Build
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -7,6 +14,7 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 import net.jspiner.epub_viewer.dto.Epub
+import net.jspiner.epub_viewer.dto.SearchResult
 import net.jspiner.epub_viewer.ui.base.BaseViewModel
 import net.jspiner.epub_viewer.ui.search.finder.PageFinder
 import net.jspiner.epubstream.dto.ItemRef
@@ -17,11 +25,12 @@ import java.util.concurrent.TimeUnit
 
 class SearchViewModel : BaseViewModel() {
 
+    private val SEARCH_RESULT_RENDER_THRESHOLD = 100
     private val textSubject = PublishSubject.create<CharSequence>()
     private lateinit var epub: Epub
     private lateinit var pageFinder: PageFinder
     private var lastRequestDisposable: Disposable? = null
-    private val searchItemSubject = PublishSubject.create<String>()
+    private val searchItemSubject = PublishSubject.create<SearchResult>()
     private val searchResetSubject = PublishSubject.create<Boolean>()
 
     init {
@@ -45,15 +54,15 @@ class SearchViewModel : BaseViewModel() {
     private fun onTextChangedInternal(content: String) {
         lastRequestDisposable?.dispose()
         searchResetSubject.onNext(true)
-        var startTime = System.currentTimeMillis()
         Observable.fromIterable(epub.opf.spine.itemrefs.toList())
             .map { toManifestItemPair(it) }
             .map { readFile(it) }
             .flatMap { findTextIndex(it, content) }
             .toFlowable(BackpressureStrategy.BUFFER)
             .flatMapSingle({ findPage(it) }, false, 1)
+            .map { toSearchResult(it, content) }
             .subscribeOn(Schedulers.computation())
-            .subscribe { println("page : " + it +" diff : " + (System.currentTimeMillis() - startTime)) }
+            .subscribe { searchItemSubject.onNext(it) }
             .also { lastRequestDisposable = it }
     }
 
@@ -115,12 +124,49 @@ class SearchViewModel : BaseViewModel() {
 
     private fun findPage(itemIndex: ItemIndex): Single<PageIndex>? {
         return pageFinder.findPage(itemIndex.itemRef, itemIndex.index)
-            .map { page -> PageIndex(page, itemIndex.index) }
+            .map { page -> PageIndex(page, itemIndex.itemRef, itemIndex.index) }
+    }
+
+    private fun toSearchResult(pageIndex: SearchViewModel.PageIndex, searchText: String): SearchResult {
+        return SearchResult(
+            toColoredSpannable(pageIndex, searchText),
+            pageIndex.page
+        )
+    }
+
+    private fun toColoredSpannable(pageIndex: SearchViewModel.PageIndex, searchText: String): Spannable {
+        val fullContent = readFile(toManifestItemPair(pageIndex.itemRef)).content
+
+        val splitStart = Math.max(0, pageIndex.index - SEARCH_RESULT_RENDER_THRESHOLD)
+        val splitEnd = Math.min(pageIndex.index + SEARCH_RESULT_RENDER_THRESHOLD, fullContent.length)
+        val innerIndex = pageIndex.index - splitStart
+
+        val splittedContent = fullContent.substring(splitStart, splitEnd)
+
+        val preContent = splittedContent.substring(0, innerIndex)
+        val renderLengthDiff = preContent.length - fromHtml(preContent).toString().trimIndent().length
+
+        val spannable = SpannableString(fromHtml(splittedContent))
+        spannable.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            (innerIndex - renderLengthDiff) - searchText.length  + 1,
+            (innerIndex - renderLengthDiff) + 1,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        return spannable
+    }
+
+    private fun fromHtml(text: String): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(text)
+        }
     }
 
     data class ItemFile(val itemRef: ItemRef, val file: File)
     data class ItemContent(val itemRef: ItemRef, val content: String)
     data class ItemIndex(val itemRef: ItemRef, val index:Int)
-    data class PageIndex(val page: Int, val index: Int)
+    data class PageIndex(val page: Int, val itemRef: ItemRef, val index: Int)
 
 }
