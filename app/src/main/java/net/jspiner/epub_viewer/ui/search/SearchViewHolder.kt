0b15009881c1dd9a6260687a3edc7f97b5483725
JSpiner <jspiner@naver.com>
2018-10-25 11:40:40 +0900

    사용하지 않는 import문 제거

--- app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchViewHolder.kt
+++ app/src/main/java/net/jspiner/epub_viewer/ui/search/SearchViewHolder.kt
@@ -2,17 +2,9 @@ package net.jspiner.epub_viewer.ui.search
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
 import android.support.v7.widget.RecyclerView
-import com.bumptech.glide.Glide
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import net.jspiner.epub_viewer.databinding.ItemLibraryBinding
 import net.jspiner.epub_viewer.databinding.ItemSearchBinding
 import net.jspiner.epub_viewer.dto.SearchResult
-import net.jspiner.epub_viewer.ui.reader.startReaderActivity
-import net.jspiner.epubstream.EpubStream
-import java.io.File
 
 class SearchViewHolder(val binding: ItemSearchBinding) : RecyclerView.ViewHolder(binding.root) {
 
