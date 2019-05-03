c97da9d8be6ac249e94e45c38924b09f597b03b3
JSpiner <jspiner@naver.com>
2019-05-03 19:27:51 +0900

    커버리지 계싼로직 추가

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,27 @@
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Hello world\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+export TRAVIS_REPO_SLUG='JSpiner/epub-viewer'
+export TRAVIS_PULL_REQUEST=35
+
+export REPORT_PATH="./app/build/reports/jacoco/jacocoTestDebugUnitTestReport/jacocoTestDebugUnitTestReport.xml"
+
+reportContent=$(cat $REPORT_PATH)
+
+missedCountList=($(echo $reportContent | grep -oP '(?<=missed=\")([^\"]*)'))
+coveredCountList=($(echo $reportContent | grep -oP '(?<=covered=\")([^\"]*)'))
+missedCountSum=0
+coveredCountSum=0
+
+for i in ${!missedCountList[*]}
+do
+    missedCountSum=$(($missedCountSum + ${missedCountList[$i]}))
+done
+
+for i in ${!coveredCountList[*]}
+do
+    coveredCountSum=$((coveredCountSum + ${coveredCountList[$i]}))
+done
+echo $coveredCountSum
+echo $missedCountSum
+
+coverage=$(echo print\($coveredCountSum / ($missedCountSum + $coveredCountSum) * 100\) | python)
+echo $coverage
+echo "done"
